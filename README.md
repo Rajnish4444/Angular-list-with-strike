ce00f1254ec7c7624d3a29596bc68a849c5cd3c9
Rajnish4444 <kaushik.rajnish98@gmail.com>
2021-07-30 19:09:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ubw7x2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ubw7x2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public name: string = 'test';
+  public dataList: { name: string; status: boolean }[] = [];
+
+  public onAdd() {
+    let tempData = { name: '', status: true };
+    tempData.name = this.name;
+    console.log(this.name);
+    this.dataList.push(tempData);
+    this.name = '';
+    console.log(this.dataList);
+  }
+
+  public strikeThis(item) {
+    let itemIndex = this.dataList.findIndex(data => data == item);
+    this.dataList[itemIndex].status = !this.dataList[itemIndex].status;
+  }
+}
